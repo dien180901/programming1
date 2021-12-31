041185d7505239b4bc97fbca9b5fbbd6b84a2fce
duysupernova <62131990+duysupernova@users.noreply.github.com>
2021-12-31 12:13:22 +0700

    Update Display.java
    Update Display.java

--- src/Display.java
+++ src/Display.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Display {
+    // Create attributes
     Summary summ;
 
+    // Create constructor for DataDisplay class
     public Display(Summary summ) {
         this.summ = summ;
     }
@@ -24,23 +26,32 @@ public class Display {
 
     // Method to display data by chart
     public void chartDisplay() {
+        // Get largest value from data
         float maxValue = MaximumValue(summ.getdisplayData());
+        // Iterate the program through y = 24 times to get y-axis
         for (int y = 0; y < 24; y++) {
             System.out.print("\n|");
             String blank = " ";
+            // Iterate through dat
             for (String value : summ.getdisplayData().get(1)) {
+                // formula to calculated the possition of data
                 int patern = 23 - Math.round(23 / maxValue * Integer.parseInt(value));
                 int a = 78 / summ.getdisplayData().get(1).size();
+                // Amount of spaces between two data points
                 int space = Math.round(a) - 1;
+                // prints '*' if line is equals to formula
                 if (patern==y) {
                     System.out.print("*");
                 }
+                // change blank to '_' to get the x-axis
                 if (y == 23) {
                     blank = "_";
                 }
+                // prints blank if line is not equals to formula
                 if (patern!=y) {
                     System.out.print(blank);
                 }
+                // create distance between two points of each line
                 while (space > 0) {
                     System.out.print(blank);
                     space--;
@@ -49,6 +60,7 @@ public class Display {
         }
     }
 
+    //Method to get largest value from data
     public int MaximumValue (ArrayList<List<String>> displayData) {
         int max = 0;
         List<String>values = displayData.get(1);
